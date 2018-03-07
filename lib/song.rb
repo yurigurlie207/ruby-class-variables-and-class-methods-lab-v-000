798d972d1a5cec6e7adf0fe126ef57a52eaee90c
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-07 18:35:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
       @@genreHash.each do |genreH, count|
         if genre == genreH
           count = count + 1
-          @@genreHash[genreH] = count + 1
+          @@genreHash[genreH] = count
         elsif
           @@genreHash[genreH] = 1
         end
