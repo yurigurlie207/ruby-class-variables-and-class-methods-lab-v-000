28d4c9ed46fdb885403dce785eb157b290896989
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-07 16:45:32 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,55 @@
+class Song
+
+  @@count = 0
+  @@genre_count = 0
+  @@artist_count = 0
+  @@genres = []
+  @@artists = []
+
+
+  def initialize(name,artist,genre)
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+    @genre = genre
+    @artist = artist
+    @name = name
+  end
+
+  def self.count
+    @@count
+  end
+
+  # attribute accessors
+  def name
+    @name
+  end
+
+  def artist
+    @artist
+  end
+
+  def genre
+    @genre
+  end
+
+ # returns arrays
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    myHash = {}
+
+    
+  end
+
+  def self.artist_count
+
+  end
+
+end
