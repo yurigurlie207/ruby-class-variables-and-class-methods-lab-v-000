2a52d542214afdc814db1ca549e06be18de99af2
Adrienne Yao <adrienne.yao@icloud.com>
2018-03-07 18:15:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   end
 
   def self.genre_count
-    @@genreHash = {@@genres[0]: 1}
+    @@genreHash = {:@@genres[0] 1}
 
     @@genres.each do |genre|
       @@genreHash.each do |genreH, count|
